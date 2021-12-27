0ccfda93d24aa7d8561f2c6dc3ee9af15c2e8579
TheKingOfCringe11 <95687516+TheKingOfCringe11@users.noreply.github.com>
2021-12-27 11:39:52 +0500

    Create Options.cs

---
+++ src/Options.cs
@@ -0,0 +1,40 @@
+﻿using DuckGame;
+
+namespace BrowseGamesPlus
+{
+    public static class Options
+    {
+        private static OptionsData _data = new OptionsData();
+
+        public static OptionsData Data => _data;
+        private static string FileName => DuckFile.optionsDirectory + "DGData.dat";
+
+        public static void Save()
+        {
+            DXMLNode data = new DXMLNode("Data");
+            data.Add(_data.Serialize());
+
+            DuckXML xml = new DuckXML();
+            xml.Add(data);
+
+            DuckFile.SaveDuckXML(xml, FileName);
+        }
+
+        public static void Load()
+        {
+            DuckXML xml = DuckFile.LoadDuckXML(FileName);
+
+            if (xml is null || xml.Elements("Data") is null)
+                return;
+
+            foreach (DXMLNode data in xml.Elements("Data").Elements())
+            {
+                if (data.Name == "Options")
+                {
+                    _data.Deserialize(data);
+                    break;
+                }
+            }
+        }
+    }
+}

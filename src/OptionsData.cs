038bde3c9b7bcb995ceffc2362d925b688a9351a
TheKingOfCringe11 <95687516+TheKingOfCringe11@users.noreply.github.com>
2021-12-27 11:39:58 +0500

    Create OptionsData.cs

---
+++ src/OptionsData.cs
@@ -0,0 +1,47 @@
+﻿using DuckGame;
+
+namespace BrowseGamesPlus
+{
+    public enum SortingMethod
+    {
+        Default,
+        Ping,
+        Maps,
+        PlayersCount,
+        Name
+    }
+
+    public class OptionsData : DataClass
+    {
+        private readonly Color[] _colors = new Color[]
+        {
+            Color.YellowGreen,
+            Color.CornflowerBlue,
+            Color.MediumPurple,
+            Color.Orange,
+            Color.Khaki,
+            Color.White
+        };
+
+        public OptionsData()
+        {
+            Players = true;
+            Avatars = true;
+            Friends = true;
+            Maps = true;
+
+            _nodeName = "Options";
+        }
+
+        public bool ConnectionRequired => Players || Avatars;
+        public SortingMethod SortingMethod => Utilities.GetEnumValueByIndex<SortingMethod>(SortingMethodIndex);
+        public Color Color => _colors[ColorIndex];
+
+        public int SortingMethodIndex { get; set; }
+        public int ColorIndex { get; set; }
+        public bool Players { get; set; }
+        public bool Avatars { get; set; }
+        public bool Friends { get; set; }
+        public bool Maps { get; set; }
+    }
+}
